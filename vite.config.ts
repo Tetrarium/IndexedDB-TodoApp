2768aa32b84630d92d1b4121132c6c4cd39c6fea
AlexandrTurov <alexandr.turov@mail.ru>
2025-05-01 21:15:22 +0700

    init project

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+    watch: {
+      usePolling: true,
+    },
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
